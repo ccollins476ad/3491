1c75623ee34b71a46e7a3e3531662f09bf35a563
Christopher Collins <ccollins@apache.org>
2016-03-31 09:16:41 -0700

    Fix smart resize bug - purple pixels.
    I don't understand why, but putpixel() was having no effect.  Changing
    the calls to _putpixel32() "solves" the problem.

--- gfx/smart_resize.c
+++ gfx/smart_resize.c
@@ -1,7 +1,8 @@
 #include <assert.h>
-#include "balleg.h"
-#include "resize.h"
-#include "smart_resize.h"
+#include "gen/gen_dbg.h"
+#include "gfx/balleg.h"
+#include "gfx/resize.h"
+#include "gfx/smart_resize.h"
 
 static int
 smart_resize_pixel_is_trans(BITMAP *bmp, int x, int y)
@@ -24,7 +25,7 @@ smart_resize_clear_trans(BITMAP *bmp)
     for (y = 0; y < bmp->h; ++y) {
         for (x = 0; x < bmp->w; ++x) {
             if (smart_resize_pixel_is_trans(bmp, x, y)) {
-                putpixel(bmp, x, y, 0);
+                _putpixel32(bmp, x, y, 0);
             }
         }
     }
@@ -45,7 +46,7 @@ smart_resize_copy_trans(BITMAP *src, BITMAP *dst)
     for (y = 0; y < src->h; ++y) {
         for (x = 0; x < src->w; ++x) {
             if (smart_resize_pixel_is_trans(src, x, y)) {
-                putpixel(dst, x, y, makecol(255, 0, 255));
+                _putpixel32(dst, x, y, makecol(255, 0, 255));
             }
         }
     }
@@ -65,16 +66,27 @@ smart_resize(struct BITMAP *src, int neww, int newh)
     stretched = create_bitmap(neww, newh);
     resized = create_bitmap(neww, newh);
 
+    set_clip_state(cleared, 0);
+    set_clip_state(stretched, 0);
+    set_clip_state(resized, 0);
+
     if (cleared == NULL || stretched == NULL || resized == NULL) {
         goto done;
     }
 
+    clear_to_color(resized, makecol(255, 0, 255));
+
+    /* cleared = source bmp with transparent replaced with black. */
     blit(src, cleared, 0, 0, 0, 0, src->w, src->h);
     smart_resize_clear_trans(cleared);
 
+    /* stretched = primitive resize of source bmp. */
     stretch_blit(src, stretched, 0, 0, src->w, src->h, 0, 0, neww, newh);
 
+    /* resized = advanced resize of cleared. */
     resize_bitmap(cleared, resized);
+
+    /* Copy transparent pixels from stretched to resized. */
     smart_resize_copy_trans(stretched, resized);
 
 done:
